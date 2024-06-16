148401571850652729120278bc3ce8834d0a2a80
Volodya <volodya.atoyan@list.ru>
2024-06-16 18:49:07 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
